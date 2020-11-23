2f4aa70d8a06cb9a9e73c14fd0056d70281f4a1e
John Henderson <john.henderson@energyweb.org>
2020-11-22 16:49:57 -0800

    Removed unused interfaces from DidTypes

--- src/did/DidTypes.ts
+++ src/did/DidTypes.ts
@@ -1,5 +1,3 @@
-import { KeyValue } from '../Interfaces/Types';
-
 /**
  * A value object representing EIP-1056 DID
  */
@@ -27,34 +25,3 @@ export class DID {
     }
   }
 }
-
-/**
- * Representation of value property from DIDAttributeChanged event
- * See EIP-1056 (https://eips.ethereum.org/EIPS/eip-1056)
- */
-export interface DIDAttributeChangedValue {
-  serviceEndpoint?: string
-  hash?: string;
-  hashAlg?: string;
-
-  publicKey?: string;
-  tag?: string;
-}
-
-/**
- * Claim as retrievable from IPFS
- */
-export interface IpfsClaim {
-  did: string;
-  signer: string;
-  claimData: {
-    fields: KeyValue[],
-    claimType: string;
-  },
-  sub: string;
-  iat: number;
-  iss: string;
-  jti: string;
-  exp: number;
-}
-
