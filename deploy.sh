d751f65dffc2e10006a5d61158de3ba97f53a4d1
Daniel Wojno <daniel.wojno@tsh.io>
2021-01-29 13:51:06 +0100

    feat(deployment): add travis deployment for production

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@
 export AWS_ACCESS_KEY_ID=$1
 export AWS_SECRET_ACCESS_KEY=$2
 export AWS_REGION=$3
-export IAM_CACHE_SERVER_IMAGE=$4
+export AWS_ECR_REPOSITORY=$4
 
 # Login to docker registry
-aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $IAM_CACHE_SERVER_IMAGE
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ECR_REPOSITORY
 
 # Creating backup
 make backup-ci
