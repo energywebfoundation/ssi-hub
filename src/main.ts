3ce077e69116c3111015f091df9ef7abfeffb9e9
Daniel Wojno <daniel.wojno@tsh.io>
2021-02-09 10:59:39 +0100

    feat(cookies): add option to store token in cookies

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  // add cookie parser to read jwt token cookie
+  app.use(cookieParser());
 
   const options = new DocumentBuilder()
     .setTitle('API')
@@ -21,7 +25,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, builtOptions);
   SwaggerModule.setup('api', app, document);
 
-  app.enableCors();
+  app.enableCors({ credentials: true, origin: true });
 
   await app.listen(process.env.NESTJS_PORT);
 }
