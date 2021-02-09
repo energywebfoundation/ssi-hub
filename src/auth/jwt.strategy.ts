90a18c5539a25894d710cf7ebae7da9d002192e6
Daniel Wojno <daniel.wojno@tsh.io>
2021-02-09 10:59:39 +0100

    feat(cookies): add option to store token in cookies

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -3,12 +3,21 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import fs from 'fs';
+import { Request } from 'express';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(configService: ConfigService) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
+        (req: Request) => {
+          if (req && req.cookies) {
+            return req.cookies.token as string;
+          }
+          return undefined;
+        },
+      ]),
       ignoreExpiration: false,
       secretOrKey: fs.readFileSync(configService.get<string>('JWT_PUBLIC_KEY')),
       algorithms: ['RS256'],
