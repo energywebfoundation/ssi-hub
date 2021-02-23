8c5d66dcd72fe533312abbb38d8f41ccc869eb0e
John Henderson <john.henderson@energyweb.org>
2021-02-23 14:19:01 -0800

    feat(didSync): add concurrency limit to refresh processing

--- src/did/did.processor.ts
+++ src/did/did.processor.ts
@@ -8,21 +8,27 @@ import { DID } from './DidTypes';
 export class DIDProcessor {
   private readonly logger: Logger;
 
-  constructor(private readonly didService: DIDService) {
+  constructor(
+    private readonly didService: DIDService
+  ) {
     this.logger = new Logger('DIDProcessor');
   }
 
-  @Process('upsertDocument')
+  @Process({ name: 'upsertDocument', concurrency: 0 })
   public async processDIDDocumentUpsert(job: Job<string>) {
     this.logger.log(`processing cache upsert for ${job.data}`);
     const did = new DID(job.data);
-    this.didService.upsertCachedDocument(did);
+    await this.didService.upsertCachedDocument(did);
   }
 
-  @Process('refreshDocument')
+  // Note that bull queue concurrency stacks for all methods in a processor:
+  // - https://github.com/nestjs/bull/issues/258
+  // - https://github.com/OptimalBits/bull/issues/1113#issuecomment-440706459
+  @Process({ name: 'refreshDocument', concurrency: 3 })
   public async processDIDDocumentRefresh(job: Job<string>) {
     this.logger.log(`processing cache refresh for ${job.data}`);
     const did = new DID(job.data);
-    this.didService.refreshCachedDocument(did);
+    await this.didService.refreshCachedDocument(did);
+    this.logger.debug(`finished cache refresh for: ${job.data}`);
   }
 }
