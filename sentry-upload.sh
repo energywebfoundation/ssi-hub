93c13839110ea063780af324fe44911c72ee2230
Daniel Wojno <daniel.wojno@tsh.io>
2021-03-01 12:26:58 +0100

    fix(main): iam cache server is not able to start

--- sentry-upload.sh
+++ sentry-upload.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-sentry-cli releases new "$SENTRY_RELEASE"
-sentry-cli releases files "$SENTRY_RELEASE" upload-sourcemaps ./dist --url-prefix '~/dist'
-sentry-cli releases finalize "$SENTRY_RELEASE"
+if [ -z "$SENTRY_RELEASE" ] 
+  then 
+    echo 'SENTRY NOT CONFIGURED TO UPLOAD'
+else 
+  sentry-cli releases new "$SENTRY_RELEASE"
+  sentry-cli releases files "$SENTRY_RELEASE" upload-sourcemaps ./dist --url-prefix '~/dist'
+  sentry-cli releases finalize "$SENTRY_RELEASE"
+
+fi
