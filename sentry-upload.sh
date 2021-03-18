2c03ab682fa12d1d03c451248edb5b370ef53f33
Daniel Wojno <daniel.wojno@tsh.io>
2021-03-18 15:36:21 +0100

    fix(sentry): sentry not displaying maps

--- sentry-upload.sh
+++ sentry-upload.sh
@@ -5,7 +5,7 @@ if [ -z "$SENTRY_RELEASE" ]
     echo 'SENTRY NOT CONFIGURED TO UPLOAD'
 else 
   sentry-cli releases new "$SENTRY_RELEASE"
-  sentry-cli releases files "$SENTRY_RELEASE" upload-sourcemaps ./dist --url-prefix '~/dist'
+  sentry-cli releases files "$SENTRY_RELEASE" upload-sourcemaps ./dist
   sentry-cli releases finalize "$SENTRY_RELEASE"
 
 fi
