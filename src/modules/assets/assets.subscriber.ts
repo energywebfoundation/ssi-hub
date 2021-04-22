c20d0b469ed8ec07200ec94cf03b017678525a38
Dmitry Fesenko <dmitry.fesenko@482.solutions>
2021-04-22 14:51:12 +0300

    feat: emit offer accept reject cancel

--- src/modules/assets/assets.subscriber.ts
+++ src/modules/assets/assets.subscriber.ts
@@ -5,12 +5,15 @@ import { Repository } from 'typeorm';
 import { Logger } from '../logger/logger.service';
 import { AssetsHistory } from './assets.entity';
 import { AssetEvent, AssetHistoryEventType } from './assets.event';
+import { NatsService } from '../nats/nats.service';
+import { NATS_EXCHANGE_TOPIC } from '../claim/claim.types';
 
 @Injectable()
 export class AssetsEventSubscriber {
   constructor(
     @InjectRepository(AssetsHistory)
     private readonly historyRepository: Repository<AssetsHistory>,
+    private readonly nats: NatsService,
     private readonly logger: Logger,
   ) {
     this.logger.setContext(AssetsEventSubscriber.name);
@@ -31,107 +34,50 @@ export class AssetsEventSubscriber {
     return Boolean(savedEvent);
   }
 
-  @OnEvent(AssetHistoryEventType.ASSET_CREATED)
-  async handleAssetCreatedEvent(event: AssetEvent) {
+  private async handleAssetEvent(event: AssetEvent, type: AssetHistoryEventType, publish = true) {
     if (
-      await this.isEventAlreadySaved(AssetHistoryEventType.ASSET_CREATED, event)
+      await this.isEventAlreadySaved(type, event)
     ) {
       return;
     }
-    const eventToSave = AssetsHistory.create({
-      ...event,
-      type: AssetHistoryEventType.ASSET_CREATED,
-    });
-    const saved = await this.historyRepository.save(eventToSave);
+
+    const saved = await this.historyRepository.save(event);
     this.logger.debug(
-      `${eventToSave.type} event for ${
-        eventToSave.assetId
-      } handled: ${JSON.stringify(saved)}`,
+      `${type} event for ${
+      event.assetId
+      } saved: ${JSON.stringify(saved)}`,
     );
+
+    if (publish) {
+      this.nats.connection.publish(
+        `${event.relatedTo}.${NATS_EXCHANGE_TOPIC}`,
+        JSON.stringify(event)
+      );
+    }
+  }
+
+  @OnEvent(AssetHistoryEventType.ASSET_CREATED)
+  async handleAssetCreatedEvent(event: AssetEvent) {
+    this.handleAssetEvent(event, AssetHistoryEventType.ASSET_CREATED, false);
   }
 
   @OnEvent(AssetHistoryEventType.ASSET_OFFER_CANCELED)
   async handleAssetCancelOfferEvent(event: AssetEvent) {
-    if (
-      await this.isEventAlreadySaved(
-        AssetHistoryEventType.ASSET_OFFER_CANCELED,
-        event,
-      )
-    ) {
-      return;
-    }
-    const eventToSave = AssetsHistory.create({
-      ...event,
-      type: AssetHistoryEventType.ASSET_OFFER_CANCELED,
-    });
-    const saved = await this.historyRepository.save(eventToSave);
-    this.logger.debug(
-      `${eventToSave.type} event for ${
-        eventToSave.assetId
-      } handled: ${JSON.stringify(saved)}`,
-    );
+    this.handleAssetEvent(event, AssetHistoryEventType.ASSET_OFFER_CANCELED);
   }
 
   @OnEvent(AssetHistoryEventType.ASSET_OFFERED)
   async handleAssetOffered(event: AssetEvent) {
-    if (
-      await this.isEventAlreadySaved(AssetHistoryEventType.ASSET_OFFERED, event)
-    ) {
-      return;
-    }
-    const eventToSave = AssetsHistory.create({
-      ...event,
-      type: AssetHistoryEventType.ASSET_OFFERED,
-    });
-    const saved = await this.historyRepository.save(eventToSave);
-    this.logger.debug(
-      `${eventToSave.type} event for ${
-        eventToSave.assetId
-      } handled: ${JSON.stringify(saved)}`,
-    );
+    this.handleAssetEvent(event, AssetHistoryEventType.ASSET_OFFERED);
   }
 
   @OnEvent(AssetHistoryEventType.ASSET_TRANSFERRED)
   async handleAssetTransferredEvent(event: AssetEvent) {
-    if (
-      await this.isEventAlreadySaved(
-        AssetHistoryEventType.ASSET_TRANSFERRED,
-        event,
-      )
-    ) {
-      return;
-    }
-    const eventToSave = AssetsHistory.create({
-      ...event,
-      type: AssetHistoryEventType.ASSET_TRANSFERRED,
-    });
-    const saved = await this.historyRepository.save(eventToSave);
-    this.logger.debug(
-      `${eventToSave.type} event for ${
-        eventToSave.assetId
-      } handled: ${JSON.stringify(saved)}`,
-    );
+    this.handleAssetEvent(event, AssetHistoryEventType.ASSET_TRANSFERRED);
   }
 
   @OnEvent(AssetHistoryEventType.ASSET_OFFER_REJECTED)
   async handleAssetOfferRejected(event: AssetEvent) {
-    if (
-      await this.isEventAlreadySaved(
-        AssetHistoryEventType.ASSET_OFFER_REJECTED,
-        event,
-      )
-    ) {
-      return;
-    }
-    const eventToSave = AssetsHistory.create({
-      ...event,
-      type: AssetHistoryEventType.ASSET_OFFER_REJECTED,
-    });
-    const saved = await this.historyRepository.save(eventToSave);
-    this.logger.debug(
-      `${eventToSave.type} event for ${
-        eventToSave.assetId
-      } handled: ${JSON.stringify(saved)}`,
-    );
+    this.handleAssetEvent(event, AssetHistoryEventType.ASSET_OFFER_REJECTED);
   }
 }
