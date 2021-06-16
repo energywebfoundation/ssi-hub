3e15003ac822d03ba04fd32b3f3c9f93ff3f011d
John Henderson <john.henderson@energyweb.org>
2021-06-16 13:11:09 -0700

    test(did.service): service defined with mock dependencies

--- src/modules/did/did.service.spec.ts
+++ src/modules/did/did.service.spec.ts
@@ -1,13 +1,76 @@
+import { getQueueToken } from '@nestjs/bull';
+import { HttpService, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { SchedulerRegistry } from '@nestjs/schedule';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Provider } from '../../common/provider';
+import { DIDDocumentEntity } from './did.entity';
 import { DIDService } from './did.service';
 
-// TODO: fix test so pending can be removed
-xdescribe('DidDocumentService', () => {
+const MockLogger = {
+  log: jest.fn(),
+  setContext: jest.fn(),
+};
+const MockObject = {};
+const MockConfigService = {
+  get: jest.fn((key: string) => {
+    if (key === 'DID_SYNC_ENABLED') {
+      return 'false';
+    }
+    return null;
+  }),
+};
+const repositoryMockFactory = jest.fn(() => ({
+  findOne: jest.fn(entity => entity),
+}));
+const queueMockFactory = jest.fn(() => ({
+}));
+jest.mock('@ew-did-registry/did-ipfs-store', () => {
+  return {
+    DidStore: jest.fn().mockImplementation(() => { return {} })
+  }
+});
+jest.mock('@ew-did-registry/did-ethr-resolver', () => ({
+  ...jest.requireActual('@ew-did-registry/did-ethr-resolver') as Record<string, unknown>,
+  Resolver: jest.fn().mockImplementation(() => { return {} })
+}));
+jest.mock('../../ethers/EthereumDidRegistryFactory', () => ({
+  EthereumDidRegistryFactory: {
+    connect: jest.fn(() => {
+      return {
+        addEventListener: jest.fn()
+      }
+    })
+  }
+}));
+
+describe('DidDocumentService', () => {
   let service: DIDService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DIDService],
+      providers: [DIDService,
+        {
+          provide: ConfigService,
+          useValue: MockConfigService
+        },
+        {
+          provide: SchedulerRegistry,
+          useValue: MockObject
+        },
+        {
+          provide: HttpService,
+          useValue: MockObject
+        },
+        {
+          provide: Logger,
+          useValue: MockLogger
+        },
+        { provide: getQueueToken('dids'), useFactory: queueMockFactory },
+        { provide: getRepositoryToken(DIDDocumentEntity), useFactory: repositoryMockFactory },
+        { provide: Provider, useValue: MockObject }
+      ]
     }).compile();
 
     service = module.get<DIDService>(DIDService);
