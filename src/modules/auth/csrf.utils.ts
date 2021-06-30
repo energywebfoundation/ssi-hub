d95222effb8da83e2cb530a35d96347c63035bfc
hrachocode <hracho94@yahoo.com>
2021-06-30 15:45:03 +0400

    improved server protection

--- src/modules/auth/csrf.utils.ts
+++ src/modules/auth/csrf.utils.ts
@@ -1,8 +1,10 @@
+import { InternalServerErrorException } from "@nestjs/common";
 import csurf from "csurf";
 import { NextFunction, Request, Response } from "express";
 
 export const checkClientCsrf = (req: Request, res: Response, next: NextFunction) => {
     if (!req.headers['server-request']) {
+        // client request with csrf protection
         const csrf = csurf({
             cookie: {
                 httpOnly: true,
@@ -11,7 +13,11 @@ export const checkClientCsrf = (req: Request, res: Response, next: NextFunction)
             }
         })
         csrf(req, res, next)
-    } else {
+    } else if (req.headers['server-request'] === process.env.CSRF_SERVER_SECRET) {
+        // validated server request
         next()
+    } else {
+        // no csrf protection and not a server request as well
+        throw new InternalServerErrorException('Something went wrong')
     }
 }
