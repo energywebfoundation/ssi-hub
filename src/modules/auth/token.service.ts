4b420a049c05285309421ca6c632da5215613003
Hrach <hracho94@yahoo.com>
2021-07-13 12:40:42 +0400

    Update src/modules/auth/token.service.ts
    Co-authored-by: John Henderson <jrhender@users.noreply.github.com>

--- src/modules/auth/token.service.ts
+++ src/modules/auth/token.service.ts
@@ -69,6 +69,20 @@ export class TokenService {
     return this.refreshTokenRepository.deleteRefreshTokenById(id);
   }
 
+/**
+ * Our approach to prevent or at least maximum decrease chances for any CSRF attacks:
+ *
+ * Provide a Protection using Origin in headers and combining double check with JWT: 
+ * * Origin is a request header supported in most modern browsers, indicating from where the request originated, **which cannot be modified** <https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Origin>
+ * * We will store the request Origin header in JWT token after login
+ * * And with every request our middleware will check if Origin in request headers is matching with origin stored in JTW token(**hacker cannot modify JWT as we are signing it with our secret in backend**)
+ * * For node requests we will check if origin in request headers is undefined. 
+ * * IF ORIGIN IS SAME OR UNDEFINED: everything is fine
+ * * IF NO: throw an exception as the origin could be from a hacker's website.
+ * * More info(check approach 2, **origin headers**): <https://security.stackexchange.com/questions/203890/how-to-implement-csrf-protection-with-a-cross-origin-request-cors/203910#203910>
+ *
+ * A pattern such as the double cookie submit pattern cannot be used because the cache-server does not share an origin with it's clients.
+ */
   async handleOriginCheck(req: Request, res: Response, next: NextFunction, service: JwtService) {
     let token = null;
     if (req.headers['authorization']) {
