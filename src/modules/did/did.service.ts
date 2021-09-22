02ad59e33133f1bed22c819e97a962371d9983db
John Henderson <jrhender@users.noreply.github.com>
2021-09-22 16:38:27 +0200

    Revert "Undo revert  #166 with fix" (#170)
    Login is still too slow

--- src/modules/did/did.service.ts
+++ src/modules/did/did.service.ts
@@ -9,12 +9,12 @@ import { IDidStore } from '@ew-did-registry/did-store-interface';
 import { HttpService, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { SchedulerRegistry } from '@nestjs/schedule';
-import { EthereumDIDRegistry__factory } from '../../ethers/factories/EthereumDIDRegistry__factory';
-import { EthereumDIDRegistry } from '../../ethers/EthereumDIDRegistry';
+import { EthereumDidRegistryFactory } from '../../ethers/EthereumDidRegistryFactory';
+import { EthereumDidRegistry } from '../../ethers/EthereumDidRegistry';
 import { InjectQueue } from '@nestjs/bull';
 import { Queue } from 'bull';
 import { DID, UPDATE_DID_DOC_QUEUE_NAME } from './did.types';
-import { BigNumber } from 'ethers';
+import { utils } from 'ethers';
 import { Logger } from '../logger/logger.service';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DIDDocumentEntity, IClaim } from './did.entity';
@@ -28,9 +28,11 @@ import {
   ethrReg,
 } from '@ew-did-registry/did-ethr-resolver';
 
+const { bigNumberify } = utils;
+
 @Injectable()
 export class DIDService {
-  private readonly didRegistry: EthereumDIDRegistry;
+  private readonly didRegistry: EthereumDidRegistry;
   private readonly ipfsStore: IDidStore;
   private readonly resolver: Resolver;
   constructor(
@@ -58,7 +60,7 @@ export class DIDService {
       method: Methods.Erc1056,
     });
 
-    this.didRegistry = EthereumDIDRegistry__factory.connect(
+    this.didRegistry = EthereumDidRegistryFactory.connect(
       DID_REGISTRY_ADDRESS,
       this.provider,
     );
@@ -195,7 +197,7 @@ export class DIDService {
 
   private async InitEventListeners(): Promise<void> {
     const DIDAttributeChanged = 'DIDAttributeChanged';
-    this.didRegistry.on(DIDAttributeChanged, async address => {
+    this.didRegistry.addListener(DIDAttributeChanged, async address => {
       const did = `did:${Methods.Erc1056}:${address}`;
 
       this.logger.info(`${DIDAttributeChanged} event received for did: ${did}`);
@@ -226,7 +228,7 @@ export class DIDService {
     // Operations on IDIDLogData expect some properties to be BigNumbers
     const bigNumberReviver = (key: string, value) => {
       if (knownBigNumberProperties.has(key) && '_hex' in value) {
-        return BigNumber.from(value['_hex']);
+        return bigNumberify(value['_hex']);
       }
       return value;
     };
@@ -264,7 +266,7 @@ export class DIDService {
    */
   private async getAllLogs(id: string): Promise<IDIDLogData> {
     const genesisBlockNumber = 0;
-    const readFromBlock = BigNumber.from(genesisBlockNumber);
+    const readFromBlock = utils.bigNumberify(genesisBlockNumber);
     return await this.resolver.readFromBlock(id, readFromBlock);
   }
 
