40713109974a8ffa3fd61332999166f13d2a57e9
John Henderson <john.henderson@energyweb.org>
2021-09-29 15:18:07 +0200

    feat(login.strategy): add optional numBlocksBack config

--- src/modules/auth/login.strategy.ts
+++ src/modules/auth/login.strategy.ts
@@ -6,11 +6,19 @@ import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class AuthStrategy extends PassportStrategy(LoginStrategy, 'login') {
   constructor(configService: ConfigService) {
-    super({
+    let loginStrategyParams = {
       name: 'login',
       rpcUrl: configService.get<string>('ENS_URL'),
       cacheServerUrl: configService.get<string>('STRATEGY_CACHE_SERVER'),
       privateKey: configService.get<string>('STRATEGY_PRIVATE_KEY'),
-    });
+    };
+    const numBlocksBack = configService.get<string>('STRATEGY_NUM_BLOCKS_BACK');
+    if (numBlocksBack) {
+      loginStrategyParams = {
+        ...loginStrategyParams,
+        ...{ numberOfBlocksBack: numBlocksBack },
+      };
+    }
+    super(loginStrategyParams);
   }
 }
