68d1a6442d20bb78fc1bf1338bf792a8aa750abb
JGiter <dimaadipmi@gmail.com>
2021-12-09 09:42:27 +0200

    feat(did): bad request on malformed did

--- src/modules/did/did.pipe.ts
+++ src/modules/did/did.pipe.ts
@@ -1,9 +1,18 @@
-import { PipeTransform, Injectable, ArgumentMetadata } from '@nestjs/common';
+import {
+  PipeTransform,
+  Injectable,
+  ArgumentMetadata,
+  BadRequestException,
+} from '@nestjs/common';
 import { DID } from './did.types';
 
 @Injectable()
 export class DIDPipe implements PipeTransform<string, DID> {
   transform(did: string, metadata: ArgumentMetadata): DID {
-    return new DID(did);
+    try {
+      return new DID(did);
+    } catch (e) {
+      throw new BadRequestException(e.message);
+    }
   }
 }
--- src/modules/did/did.types.ts
+++ src/modules/did/did.types.ts
@@ -1,11 +1,17 @@
-import { Chain, Methods } from '@ew-did-registry/did';
+import { Methods } from '@ew-did-registry/did';
+import { MalformedDIDError } from '../../errors/MalformedDIDErrro';
 
+// did-reg Chain is lowercase
+export enum Chain {
+  VOLTA = 'VOLTA',
+  EWC = 'EWC',
+}
 /**
  * @todo >> ew-did-registry/did
  * matches did:ethr:volta:address and did:ethr:address
  */
 export const didPattern =
-  '^(?:did:(?<method>[a-z0-9]+?):)(?<chain>[a-z0-9]+?:)?(?<id>0x[A-Fa-f0-9]{40})$';
+  '^(?:did:(?<method>[a-z0-9]+?):)((?<chain>[a-zA-Z0-9]+?):)?(?<id>0x[A-Fa-f0-9]{40})$';
 
 /**
  * A value object representing EIP-1056 DID
@@ -25,15 +31,15 @@ export class DID {
   constructor(did: string) {
     const match = did.match(didPattern);
     if (!match) {
-      throw new Error(`${did} is malformed DID`);
+      throw new MalformedDIDError(did);
     }
-    const { method, chain = <Chain>process.env.CHAIN_NAME, id } = <
-      { method: Methods; chain?: Chain; id: string }
+    const { method, chain = process.env.CHAIN_NAME, id } = <
+      { method: Methods; chain?: string; id: string }
     >match.groups;
     this.method = method;
-    this.chain = chain;
+    this.chain = <Chain>chain.toUpperCase();
     this.id = id;
-    this.did = did;
+    this.did = `did:${this.method}:${this.chain}:${this.id}`;
   }
 }
 
