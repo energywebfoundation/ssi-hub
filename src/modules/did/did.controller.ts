9067b5e875ac0b6260c4453aa6bf099344dee419
JGiter <dimaadipmi@gmail.com>
2021-12-09 10:17:43 +0200

    feat(did): default chain for ethr did

--- src/modules/did/did.controller.ts
+++ src/modules/did/did.controller.ts
@@ -5,7 +5,7 @@ import { Logger } from '../logger/logger.service';
 import { Auth } from '../auth/auth.decorator';
 import { NotFoundInterceptor } from '../interceptors/not-found.interceptor';
 import { DIDService } from './did.service';
-import { DID, getDIDFromAddress } from './did.types';
+import { DID } from './did.types';
 import { DIDPipe } from './did.pipe';
 
 @Auth()
@@ -35,22 +35,12 @@ export class DIDController {
   })
   @UseInterceptors(NotFoundInterceptor)
   public async getById(@Param('did', DIDPipe) did: DID) {
-    this.logger.info(`Received request for document for did: ${did}`);
+    this.logger.info(`Received request for document for did: ${did.did}`);
 
-    // If an ethr DID is received that doesn't have chain info, then add it.
-    // This is because we want to be backwards compatible with EWF code that didn't include the chain info in the DID
-    // But we want to add the chain info because the DID resolution is actually occurring from a specific chain
-    let didToRetrieve: string;
-    if (did.method === 'ethr' && did.chain === undefined) {
-      didToRetrieve = getDIDFromAddress(did.id);
-    } else {
-      didToRetrieve = did.did;
-    }
-
-    this.logger.info(`Retrieving document for did: ${didToRetrieve}`);
+    this.logger.info(`Retrieving document for did: ${did.did}`);
     if (did.method !== 'ethr') {
-      return this.didService.getDIDDocumentFromUniversalResolver(didToRetrieve);
+      return this.didService.getDIDDocumentFromUniversalResolver(did.did);
     }
-    return this.didService.getById(didToRetrieve);
+    return this.didService.getById(did.did);
   }
 }
--- src/modules/did/did.types.ts
+++ src/modules/did/did.types.ts
@@ -1,17 +1,12 @@
-import { Methods } from '@ew-did-registry/did';
+import { Methods, Chain } from '@ew-did-registry/did';
 import { MalformedDIDError } from '../../errors/MalformedDIDErrro';
 
-// did-reg Chain is lowercase
-export enum Chain {
-  VOLTA = 'VOLTA',
-  EWC = 'EWC',
-}
 /**
  * @todo >> ew-did-registry/did
- * matches did:ethr:volta:address and did:ethr:address
+ * matches did:ethr:volta:address, did:ethr:vOLTa:address and did:ethr:address
  */
 export const didPattern =
-  '^(?:did:(?<method>[a-z0-9]+?):)((?<chain>[a-zA-Z0-9]+?):)?(?<id>0x[A-Fa-f0-9]{40})$';
+  '^(?:did:(?<method>[a-z0-9]+?):)((?<chain>[a-z0-9]+?):)?(?<id>0x[A-Fa-f0-9]{40})$';
 
 /**
  * A value object representing EIP-1056 DID
@@ -33,13 +28,22 @@ export class DID {
     if (!match) {
       throw new MalformedDIDError(did);
     }
-    const { method, chain = process.env.CHAIN_NAME, id } = <
-      { method: Methods; chain?: string; id: string }
-    >match.groups;
-    this.method = method;
-    this.chain = <Chain>chain.toUpperCase();
+    const { method, id } = match.groups;
+    let chain = match.groups.chain;
+    if (!chain && method === Methods.Erc1056) {
+      chain = process.env.CHAIN_NAME;
+    }
+    this.method = <Methods>method;
+    this.chain = <Chain>chain;
     this.id = id;
-    this.did = `did:${this.method}:${this.chain}:${this.id}`;
+    // If an ethr DID is received that doesn't have chain info, then add it.
+    // This is because we want to be backwards compatible with EWF code that didn't include the chain info in the DID
+    // But we want to add the chain info because the DID resolution is actually occurring from a specific chain
+    if (this.chain) {
+      this.did = `did:${this.method}:${this.chain}:${this.id}`;
+    } else {
+      this.did = `did:${this.method}:${this.id}`;
+    }
   }
 }
 
