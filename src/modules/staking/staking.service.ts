fe3279a472e069e09c896f04e95543b61d625c98
Krzysztof Szostak <christopher.szostak@energyweb.org>
2021-12-14 13:15:32 +0100

    fix: remove staking

--- src/modules/staking/staking.service.ts
+++ src/modules/staking/staking.service.ts
@@ -1,45 +1,20 @@
-import {
-  BadRequestException,
-  Injectable,
-  OnModuleDestroy,
-} from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { StakingPool } from './entities/staking.pool.entity';
 import { StakingTerms } from './entities/staking.terms.entity';
 import { Logger } from '../logger/logger.service';
-import { StakingPoolFactory__factory } from '../../ethers/factories/StakingPoolFactory__factory';
-import { StakingPoolFactory } from '../../ethers/StakingPoolFactory';
-import { Provider } from '../../common/provider';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class StakingService implements OnModuleDestroy {
-  private stakingPoolFactory: StakingPoolFactory;
+export class StakingService {
   constructor(
     @InjectRepository(StakingTerms)
     private readonly stakingTermsRepository: Repository<StakingTerms>,
     @InjectRepository(StakingPool)
     private readonly stakingPoolRepository: Repository<StakingPool>,
     private readonly logger: Logger,
-    private readonly provider: Provider,
-    config: ConfigService,
   ) {
     this.logger.setContext(StakingService.name);
-
-    const STAKING_POOL_FACTORY_ADDRESS = config.get<string>(
-      'STAKING_POOL_FACTORY_ADDRESS',
-    );
-
-    if (!STAKING_POOL_FACTORY_ADDRESS) {
-      this.logger.warn("'STAKING_POOL_FACTORY_ADDRESS' is not specified.");
-    }
-
-    this.stakingPoolFactory = StakingPoolFactory__factory.connect(
-      STAKING_POOL_FACTORY_ADDRESS,
-      this.provider,
-    );
-    this.InitEventListeners();
   }
 
   async saveTerms(stakeTerms: Partial<StakingTerms>): Promise<StakingTerms> {
@@ -86,14 +61,5 @@ export class StakingService implements OnModuleDestroy {
       terms,
       address,
     });
-  }
-  private InitEventListeners(): void {
-    this.stakingPoolFactory.on('StakingPoolLaunched', async (_, address) => {
-      await this.saveStakingPool(address);
-    });
-  }
-
-  onModuleDestroy() {
-    this.stakingPoolFactory.removeAllListeners('StakingPoolLaunched');
   }
 }
