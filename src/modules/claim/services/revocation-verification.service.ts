0b97431eb38cd5b5127beb75826f9dfe24578e7d
whitneypurdum <whitney.purdum@energyweb.org>
2022-09-29 11:57:51 -0400

    fix: add logging for role credential resolver

--- src/modules/claim/services/revocation-verification.service.ts
+++ src/modules/claim/services/revocation-verification.service.ts
@@ -1,4 +1,4 @@
-import { RevocationVerification } from '@energyweb/vc-verification';
+import { RevocationVerification, Ipfs } from '@energyweb/vc-verification';
 import { Inject, Injectable } from '@nestjs/common';
 import { verifyCredential } from 'didkit-wasm-node';
 import { RegistrySettings } from '@ew-did-registry/did-resolver-interface';
@@ -8,6 +8,7 @@ import { RoleCredentialResolver } from '../resolvers/credential.resolver';
 import { RoleIssuerResolver } from '../resolvers/issuer.resolver';
 import { RoleRevokerResolver } from '../resolvers/revoker.resolver';
 import { DIDService } from '../../did/did.service';
+import { Logger } from '../../logger/logger.service';
 
 @Injectable()
 export class RevocationVerificationService extends RevocationVerification {
@@ -15,11 +16,12 @@ export class RevocationVerificationService extends RevocationVerification {
     didService: DIDService,
     roleService: RoleService,
     provider: Provider,
-    @Inject('RegistrySettings') registrySettings: RegistrySettings
+    @Inject('RegistrySettings') registrySettings: RegistrySettings,
+    logger: Logger
   ) {
     const issuerResolver = new RoleIssuerResolver(roleService);
     const revokerResolver = new RoleRevokerResolver(roleService);
-    const credentialResolver = new RoleCredentialResolver(didService);
+    const credentialResolver = new RoleCredentialResolver(didService, logger);
     super(
       revokerResolver,
       issuerResolver,
