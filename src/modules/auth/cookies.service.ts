dd82e8f84b4880560da2a3b267db79e0ad2c2745
nichonien <ashish.tripathi@energyweb.org>
2023-02-08 15:17:13 +0530

    fix: remove chrome specific cookies update

--- src/modules/auth/cookies.service.ts
+++ src/modules/auth/cookies.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CookieOptions } from 'express';
-import useragent from 'useragent';
 
 @Injectable()
 export class CookiesServices {
@@ -10,11 +9,6 @@ export class CookiesServices {
       sameSite: 'none',
       secure: true,
     };
-    const { family, major } = useragent.parse(userAgent) || {};
-    if (family === 'Chrome' && +major >= 51 && +major <= 66) {
-      delete cookiesOptions.sameSite;
-      delete cookiesOptions.secure;
-    }
     return cookiesOptions;
   }
 }
