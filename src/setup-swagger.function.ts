4c40aa6fad144e5b621b0c6d5828e32e84df2632
Artur Sudnik-Hrynkiewicz <artur.sudnik@gmail.com>
2023-03-30 12:02:41 +0200

    feat: adding setting of Swagger version taken from the `package.json`

--- src/setup-swagger.function.ts
+++ src/setup-swagger.function.ts
@@ -1,12 +1,14 @@
 import { INestApplication } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
 
 export const setupSwagger = (app: INestApplication, config: ConfigService) => {
   const options = new DocumentBuilder()
     .setTitle('API')
     .setDescription('Cache Server API documentation')
-    .setVersion('1.0');
+    .setVersion(readVersion());
 
   if (config.get<boolean>('ENABLE_AUTH')) {
     options.addBearerAuth();
@@ -18,3 +20,18 @@ export const setupSwagger = (app: INestApplication, config: ConfigService) => {
     SwaggerModule.createDocument(app, options.build())
   );
 };
+
+function readVersion(): string {
+  let pkg;
+
+  try {
+    pkg = JSON.parse(
+      readFileSync(resolve(__dirname, '../package.json')).toString('utf8')
+    );
+  } catch (err) {
+    console.log(`error reading/parsing package.json: ${err}`);
+    return '';
+  }
+
+  return pkg.version;
+}
