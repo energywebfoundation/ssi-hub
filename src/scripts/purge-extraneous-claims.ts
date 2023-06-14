dc7b12da2b4b68963a2200ff01cb6ae495575cfb
JGiter <dimaadipmi@gmail.com>
2023-06-14 15:06:36 +0300

    fix: specify only connection option to purge claims

--- src/scripts/purge-extraneous-claims.ts
+++ src/scripts/purge-extraneous-claims.ts
@@ -3,10 +3,18 @@
  */
 
 import { inspect } from 'util';
-import { dataSource } from '../db/cli';
 import { RoleClaim } from '../modules/claim/entities/roleClaim.entity';
+import { DataSource } from 'typeorm';
 
 (async function () {
+  const dataSource = new DataSource({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
   dataSource.setOptions({ entities: [RoleClaim] });
   await dataSource.initialize();
 
