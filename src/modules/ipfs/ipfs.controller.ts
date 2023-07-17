f8c08d06cf4414aa4107081434b753b2915352b9
Dmitry <44746858+JGiter@users.noreply.github.com>
2023-07-17 09:15:52 +0300

    docs: ipfs get
    Co-authored-by: John Henderson <jrhender@users.noreply.github.com>

--- src/modules/ipfs/ipfs.controller.ts
+++ src/modules/ipfs/ipfs.controller.ts
@@ -22,8 +22,8 @@ export class IPFSController {
   @Get('/:cid')
   @ApiTags('IPFS')
   @ApiOperation({
-    summary: 'Returns credential from IPFS Store',
-    description: 'Returns credential represented by service in DID document.',
+    summary: 'Returns content from IPFS Store',
+    description: 'Returns data identified by the provided content identifier (cid). This can be used, for example, to resolve credentials linked in the service endpoint of a DID document.',
   })
   @ApiParam({ name: 'cid', type: 'string', required: true })
   public async get(@Param('cid', CIDPipe) cid: CID): Promise<string> {
