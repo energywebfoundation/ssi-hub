58cad0e6f0dfdaf5b0ae83c6f5e108e901079fbc
azam-ismail <66775486+azam-ismail@users.noreply.github.com>
2025-05-26 10:56:05 +0800

    Fix/pinning service remove (#689)
    * fix: remove ipfs pinning service
    
    * fix: add jobId using cid
    
    * fix: just remove from queue

--- src/modules/did/did.processor.ts
+++ src/modules/did/did.processor.ts
@@ -1,22 +1,15 @@
 import {
-  InjectQueue,
   OnQueueActive,
   OnQueueError,
   OnQueueFailed,
   OnQueueStalled,
   OnQueueWaiting,
   Process,
-  Processor,
+  Processor
 } from '@nestjs/bull';
 import { ConfigService } from '@nestjs/config';
-import { Job, Queue } from 'bull';
-import {
-  PinClaimData,
-  PIN_CLAIM_JOB_NAME,
-  PIN_CLAIM_QUEUE_NAME,
-} from '../ipfs/ipfs.types';
+import { Job } from 'bull';
 import { Logger } from '../logger/logger.service';
-import { DIDDocumentEntity } from './did.entity';
 import { DIDService } from './did.service';
 import {
   ADD_DID_DOC_JOB_NAME,
@@ -30,8 +23,6 @@ export class DIDProcessor {
     private readonly didService: DIDService,
     private readonly logger: Logger,
     private readonly configService: ConfigService,
-    @InjectQueue(PIN_CLAIM_QUEUE_NAME)
-    private pinQueue: Queue<PinClaimData>
   ) {
     this.logger.setContext(DIDProcessor.name);
   }
@@ -63,38 +54,15 @@ export class DIDProcessor {
 
   @Process(ADD_DID_DOC_JOB_NAME)
   public async processDIDDocumentAddition(job: Job<string>) {
-    const doc = await this.didService.addCachedDocument(job.data);
-    await this.addPinJobs(doc);
+    await this.didService.addCachedDocument(job.data);
   }
 
   @Process(UPDATE_DID_DOC_JOB_NAME)
   public async processDIDDocumentRefresh(job: Job<string>) {
-    let doc: DIDDocumentEntity;
     if (this.configService.get<boolean>('DID_SYNC_MODE_FULL')) {
-      doc = await this.didService.addCachedDocument(job.data, true);
+      await this.didService.addCachedDocument(job.data, true);
     } else {
-      doc = await this.didService.incrementalRefreshCachedDocument(job.data);
+      await this.didService.incrementalRefreshCachedDocument(job.data);
     }
-    await this.addPinJobs(doc);
-  }
-
-  private async addPinJobs(doc: DIDDocumentEntity) {
-    // Cluster pinning needs to be explictly set to true
-    if (!this.configService.get<boolean>('IPFS_CLUSTER_PINNING_ENABLED')) {
-      this.logger.debug(
-        `IPFS cluster pinning disabled. Skipping for ${doc?.id}`
-      );
-      return;
-    }
-    this.logger.debug(
-      `IPFS cluster pinning enabled. Pinning IPFS data for ${doc?.id}`
-    );
-    await Promise.all(
-      doc.service.map(({ serviceEndpoint }) => {
-        this.pinQueue.add(PIN_CLAIM_JOB_NAME, {
-          cid: serviceEndpoint,
-        });
-      })
-    );
   }
 }
